2377c68751530eab49e8de3ec583198279c2776e
Gaurav Narula <gnarula94@gmail.com>
2021-04-01 11:52:57 +0200

    Use version substring and reload systemd daemon

--- build-deb.sh
+++ build-deb.sh
@@ -9,7 +9,7 @@ mkdir -p deb/opt/twins/bin     # install dir
 # binary built by CI
 mv twins-demo-reset deb/opt/twins/bin
 
-VERSION=$(./twins-demo-reset -v)
+VERSION=$(./twins-demo-reset -v | tr - +)
 
 # add systemd units
 cp -a pkg/usr deb
@@ -18,7 +18,7 @@ cp -a pkg/usr deb
 find deb ! -perm -a+r -exec chmod a+r {} \;
 
 fpm \
-    --force -t deb -a all -s dir -C deb -n twins-demo-reset -v ${VERSION} \
+    --force -t deb -a all -s dir -C deb -n twins-demo-reset -v ${VERSION:1} \
     --before-install pkg/before-install.sh \
     --after-install pkg/after-install.sh \
     --before-remove pkg/before-remove.sh \
--- pkg/after-install.sh
+++ pkg/after-install.sh
@@ -5,5 +5,6 @@ chown dedis:dedis /opt/twins
 chmod 755 /opt/twins
 
 
+su -c "systemctl --user daemon-reload" dedis
 su -c "systemctl --user start twins.target" dedis
 su -c "systemctl --user start twins_demo_reset.service" dedis
