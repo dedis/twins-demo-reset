ff3baf80cbc3a6c01c325dad8439c03a8002d95d
Gaurav Narula <gnarula94@gmail.com>
2021-04-08 11:24:42 +0200

    Enable systemd units on restart

--- pkg/after-install.sh
+++ pkg/after-install.sh
@@ -11,4 +11,6 @@ loginctl enable-linger dedis
 
 su -c "XDG_RUNTIME_DIR=/run/user/${DEDIS_UID} systemctl --user daemon-reload" dedis
 su -c "XDG_RUNTIME_DIR=/run/user/${DEDIS_UID} systemctl --user start twins.target" dedis
+su -c "XDG_RUNTIME_DIR=/run/user/${DEDIS_UID} systemctl --user enable twins.target" dedis
 su -c "XDG_RUNTIME_DIR=/run/user/${DEDIS_UID} systemctl --user start twins_demo_reset.service" dedis
+su -c "XDG_RUNTIME_DIR=/run/user/${DEDIS_UID} systemctl --user enable twins_demo_reset.service" dedis
